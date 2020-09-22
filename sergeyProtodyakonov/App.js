343f93a57f2fe21a7b6b4bb42497f8c4f405b333
Rekfuby <trollebane@gmail.com>
2020-09-22 19:28:54 +0900

    Added view colored boxes

--- sergeyProtodyakonov/App.js
+++ sergeyProtodyakonov/App.js
@@ -17,58 +17,46 @@ import {
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
   Colors,
-  DebugInstructions,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 const App: () => React$Node = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    
+  <View
+    style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+
+  <View
+  style = {styles.blockRow}>
+    <View
+    style = {styles.rectangleBlack}>
+    
+    </View>
+    <View
+    style = {styles.rectangleOrange}>
+
+    </View>
+  </View>
+
+  <View
+  style = {styles.blockRow}>
+    <View
+    style = {styles.rectangleGreen}>
+
+    </View>
+    <View
+    style = {styles.rectangleCyan}>
+
+    </View>
+  </View>
+
+  </View>
+  
+    
   );
 };
 
@@ -76,39 +64,37 @@ const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
   body: {
     backgroundColor: Colors.white,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  rectangleBlack: {
+    width: 120,
+    height: 120,
+    backgroundColor: "#000000",
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+
+  rectangleGreen: {
+    width: 120,
+    height: 120,
+    backgroundColor: "#00FF00",
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+
+  rectangleCyan: {
+    width: 120,
+    height: 120,
+    backgroundColor: "#00FFFF",
   },
-  highlight: {
-    fontWeight: '700',
+
+  rectangleOrange: {
+    width: 120,
+    height: 120,
+    backgroundColor: "#FFA500",
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+
+  blockRow: {
+    flexDirection: 'row',
   },
+
 });
 
 export default App;
