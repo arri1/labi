4919293734d2d54c05508b921098467f7203cef5
Vanya <svfuivan2@gmail.com>
2020-09-24 08:57:58 +0900

    changing colors on buttons press

--- SushkinVanya/App.js
+++ SushkinVanya/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -6,6 +6,8 @@ import {
   View,
   Text,
   StatusBar,
+  Button,
+  Alert,
 } from 'react-native';
 
 import {
@@ -16,23 +18,48 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+
+
+
+
 const App: () => React$Node = () => {
-  let numbers = [1,2,3];
-
-
+  //let numbers = [1,2,3];
+  const [buttonColor, setButtonColor] = useState('rgb(255, 0, 255)');
+  const [colorIndex, setColorIndex] = useState(0);
+  
+  
+  
   return (
+    <View>
+      <Text>yare yare daze</Text>
+      
+      <Button title="нажми меня чтоб поменять цвет"
+      color={buttonColor}
+      onPress={() => setButtonColor("#F0000F")}/>
+      <Button title="нажми меня чтоб поменять цвет"
+      color={buttonColor}
+      onPress={() => setButtonColor("#F0F00F")}/>
+      <Button title="нажми меня чтоб поменять цвет"
+      color={buttonColor}
+      onPress={() => setButtonColor("#0FF000")}/>
+      <Button title="нажми меня чтоб поменять цвет"
+      color={buttonColor}
+      onPress={() => setButtonColor("#00000F")}/>
 
+    </View>
 
-    <>
+//color={buttonColor}
 
+    /*<>
+      <button></button>
       <StatusBar/>
       <SafeAreaView>
         <ScrollView>
           <Text>yare yare daze</Text>
         </ScrollView>
       </SafeAreaView>
-    </>
-  );
+    </>*/
+ );
 };
 
 
