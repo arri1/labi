00105fb72536bdcf121012954aa13552e434f30c
ay-ay-vasilev <ay.ay.vasilev@gmail.com>
2020-09-23 19:54:05 +0900

    Deleted prettier js file and used json instead. Made all function into arrow functions, deleted myApp folder that was added by accident previously

--- aitalVasilev/App.js
+++ aitalVasilev/App.js
@@ -1,41 +1,40 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet, Button, Alert} from 'react-native';
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, Button, Alert } from 'react-native'
 
 const App = () => {
-  const [colorState, setColorState] = useState('green');
+    const [colorState, setColorState] = useState('green')
 
-  function onGreenButton() {
-    setColorState('green');
-  }
-  function onRedButton() {
-    setColorState('red');
-  }
+    onGreenButton = () => {
+        setColorState('green')
+    }
+    onRedButton = () => {
+        setColorState('red')
+    }
 
-  return (
-    <View style={styles.container}>
-      <Text>Test app</Text>
+    return (
+        <View style={styles.container}>
+            <Text>Test app</Text>
 
-      <View
-        style={{
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-          backgroundColor: colorState,
-        }}
-      />
-      <Button title="Make color red" color="red" onPress={onRedButton} />
-      <Button title="Make color green" color="green" onPress={onGreenButton} />
-    </View>
-  );
-};
+            <View style={(styles.testView, { backgroundColor: colorState })} />
+            <Button title="Make color red" color="red" onPress={onRedButton} />
+            <Button title="Make color green" color="green" onPress={onGreenButton} />
+        </View>
+    )
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    testView: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        backgroundColor: colorState
+    }
+})
 
-export default App;
+export default App
