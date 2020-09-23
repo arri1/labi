d3b3e321ccbc7e7aab14dbe133b9e62e5b0209a1
fiitt <vvnfiit@gmail.com>
2020-09-23 22:37:22 +0900

    modified App.js and deleted myApp

--- VaseninVladislav/App.js
+++ VaseninVladislav/App.js
@@ -6,71 +6,95 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
-  StatusBar,
+  TouchableOpacity
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
   Colors,
-  DebugInstructions,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App= () => {
-  return (
-    <View>
-      <Text>
-        text
-      </Text>
-    </View>
-  );
-};
 
 const styles = StyleSheet.create({
+
   scrollView: {
     backgroundColor: Colors.lighter,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
+
   body: {
     backgroundColor: Colors.white,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+
 });
+
+const App = () => {
+
+  const [color, setColor] = useState('green');
+  const [count, setCount] = useState(0);
+
+  let getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+  };
+
+  let randomColor = () => {
+    let x = getRandomInt(4);
+    switch (x) {
+      case 0:
+        setColor('yellow')
+        break;
+      case 1:
+        setColor('green')
+        break;
+      case 2:
+        setColor('red')
+        break;
+      case 3:
+        setColor('blue')
+        break;
+
+      default:
+    }
+  };
+
+  return (
+
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+
+      <TouchableOpacity
+        onPress={() => {
+          randomColor();
+          setCount(count + 1);
+        }}>
+
+        <View
+          style={{
+            backgroundColor: color,
+            width: 300,
+            height: 300
+          }}>
+          <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text style={{ fontSize: 50 }}>{count}</Text>
+          </View>
+        </View>
+
+      </TouchableOpacity>
+
+
+    </View>
+
+
+  );
+};
 
 export default App;
