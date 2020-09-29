65ad9914c0e7578c519a1e4b1176dbad7a4dc71f
ay-ay-vasilev <ay.ay.vasilev@gmail.com>
2020-09-29 18:34:12 +0900

    Removed test color value

--- aitalVasilev/App.js
+++ aitalVasilev/App.js
@@ -32,8 +32,7 @@ const styles = StyleSheet.create({
     testView: {
         position: 'absolute',
         width: '100%',
-        height: '100%',
-        backgroundColor: 'blue'
+        height: '100%'
     }
 })
 
