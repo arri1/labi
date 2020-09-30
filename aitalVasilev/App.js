16174f0b779ac7d934f9c906abc3b4ae9a499774
ay-ay-vasilev <ay.ay.vasilev@gmail.com>
2020-09-30 20:14:38 +0900

    Solved previous issues: sorted imports, changed prettier settings to make more lines and removed unnecessary empty lines. Customized the app: added the stack navigator, moved navigation to a different file, cleared the App.js, created the src folder with all the files, added icons to the bottom navigation, added React Paper and implemented the To Do items as Cards with Checkboxes. Moved the todo item to a dedicated component.

--- aitalVasilev/App.js
+++ aitalVasilev/App.js
@@ -1,21 +1,10 @@
 import 'react-native-gesture-handler'
 import React from 'react'
-import Lab2 from './screens/Lab2'
-import Lab3 from './screens/Lab3'
-import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-
-const Tab = createBottomTabNavigator()
+// Custom
+import MainNavigation from './src/navigation/MainNavigation'
 
 const App = () => {
-    return (
-        <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Lab2" component={Lab2} options={{ title: 'Lab 2' }} />
-                <Tab.Screen name="Lab3" component={Lab3} options={{ title: 'Lab 3' }} />
-            </Tab.Navigator>
-        </NavigationContainer>
-    )
+    return <MainNavigation />
 }
 
 export default App
