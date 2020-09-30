c6c754986953b7a776561f80dbcaee601b098212
0dj3 <xaryshal@gmail.com>
2020-09-30 13:28:46 +0900

    Hours

--- vladimirInnokentiev/App.js
+++ vladimirInnokentiev/App.js
@@ -1,32 +1,162 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, TouchableOpacity, View, Dimensions, StatusBar, Animated } from "react-native";
+import dayjs from 'dayjs'
+import { transform } from "@babel/core";
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const onPress = () => setCount(prevCount => prevCount + 1);
+const {width} = Dimensions.get('screen');
+const SIZE = width * 0.9;
+const TICK_INVERAL = 1000;
 
-  return (
-    <View style={styles.container}>
-        <Text>Сифра: {count}</Text>
-      <TouchableOpacity style={styles.button} onPress={onPress}>
-        <Text>Кнопка</Text>
-      </TouchableOpacity>
-    </View>
-  );
+export default class App extends React.Component{
+  state = {
+    index: new Animated.Value(0),
+    tick: new Animated.Value(0),
+    scales: [...Array(6).keys()].map(() => new Animated.Value(0))
+  };
+  
+  _timer = 0;
+  _ticker = null;
+
+  componentDidMount(){
+    const current = dayjs();
+    const diff = current.endOf('day').diff(current, 'seconds');
+    const oneDay = 24 * 60 * 60;
+
+    this._timer = oneDay - diff;
+    this.state.tick.setValue(this._timer);
+    this.state.index.setValue(this._timer - 30);
+
+    this._animate();
+
+    this._ticker = setInterval(() => {
+      this._timer += 1;
+      this.state.tick.setValue(this._timer);
+    }, TICK_INVERAL)
+  }
+
+  componentWillUnmount(){
+    clearInterval(this._ticker);
+    this._ticker = null
+  }
+
+  _animate = () => {
+    const scaleStaggerAnimations = this.state.scales.map(animated => {
+      return Animated.spring(animated, {
+        toValue: 1,
+        tension: 18,
+        friction: 3,
+        useNativeDriver: true
+      });
+    });
+
+    Animated.parallel([
+      Animated.stagger(TICK_INVERAL / this.state.scales.length, scaleStaggerAnimations),
+      Animated.timing(this.state.index, {
+        toValue: this.state.tick,
+        duration: TICK_INVERAL / 2,
+        useNativeDriver: true
+      })
+    ]).start()
+  };
+
+  render() {
+    const{index, scales: [smallQuadranScale, mediumQuadranScale, bigQuadranScale, secondsScale, minutesScale, hoursScale]} = this.state;
+
+    const interpolated = {
+      inputRange: [0,360],
+      outputRange: ['0deg', '360deg'],
+    }
+    const secondDegrees = Animated.multiply(index, 6)
+    const transformSeconds = {
+      transform: [{rotate: secondDegrees.interpolate(interpolated)}, {scale: secondsScale}]
+    };
+
+    const rotateMinutes = Animated.divide(secondDegrees,new Animated.Value(60));
+    const transformMinutes = {
+      transform: [{rotate: rotateMinutes.interpolate(interpolated)}, {scale: minutesScale}]
+    };
+
+    const rotateHours = Animated.divide(rotateMinutes, new Animated.Value(12));
+    const transformHours = {
+      transform: [{rotate: rotateHours.interpolate(interpolated)}, {scale: hoursScale}]
+    };
+    return (
+      <View style = {styles.container}>
+        <StatusBar hidden={true}/>
+        <Animated.View style = {[styles.bigQuadran], {transform: [{scale:bigQuadranScale}]}}/>
+        <Animated.View style = {[styles.mediumQuadran], {transform: [{scale:mediumQuadranScale}]}}/>
+        <Animated.View style = {[styles.smallQuadran], {transform: [{scale:smallQuadranScale}]}}/>
+        <Animated.View style = {[styles.mover, transformHours]}>
+          <View style = {[styles.hours]}/>
+        </Animated.View>
+        <Animated.View style = {[styles.mover, transformMinutes]}>
+          <View style = {[styles.minutes]}/>
+        </Animated.View>
+        <Animated.View style = {[styles.mover, transformSeconds]}>
+          <View style = {[styles.seconds]}/>
+        </Animated.View>
+       
+      </View>
+    );
+  };
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    paddingHorizontal: 10
+    backgroundColor: "#fff"
   },
-  button: {
-    alignItems: "center",
-    backgroundColor: "#0e60e6",
-    padding: 10
-  }
+  mover: {
+    position: 'absolute',
+    width: SIZE,
+    height: SIZE,
+    borderRadius: SIZE/2,
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  hours: {
+    
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    height: '35%',
+    marginTop: '15%',
+    width: 4,
+    borderRadius: 4
+  },
+  minutes: {
+    
+    backgroundColor: 'rgba(0,0,0,0.8)',
+    height: '45%',
+    marginTop: '5%',
+    width: 3,
+    borderRadius: 3
+  },
+  seconds: {
+    
+    backgroundColor: 'rgba(227,71,134,1)',
+    height: '50%',
+    width: 2,
+    borderRadius: 2
+  },
+  bigQuadran: {
+    position: 'absolute',
+    width: SIZE * 0.8,
+    height: SIZE * 0.8,
+    borderRadius: SIZE * 0.4,
+    backgroundColor: 'rgba(200,200,200,0.2)'
+  },
+  mediumQuadran: {
+    position: 'absolute',
+    width: SIZE * 0.4,
+    height: SIZE * 0.4,
+    borderRadius: SIZE * 0.25,
+    backgroundColor: 'rgba(200,200,200,0.2)'
+  },
+  smallQuadran: {
+    position: 'absolute',
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: 'rgba(227,71,134,1)'
+  },
 });
-
-export default App;
