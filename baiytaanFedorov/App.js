19baec7a4df933b6ba0ccdc6aaf9b49fd102523c
nutunabe <baiytaan@gmail.com>
2020-09-30 14:17:25 +0900

    touchableOpacity

--- baiytaanFedorov/App.js
+++ baiytaanFedorov/App.js
@@ -1,16 +1,48 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  View
+} from 'react-native';
 
-const HelloWorldApp = () => {
+const App = () => {
+  const [backgroundColor, setBackgroundColor] = useState('white')
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-      <Text>Baiytaan says: "Hi, there!"</Text>
+    <View style={[styles.body, { backgroundColor }]}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          if (backgroundColor == 'white') {
+            setBackgroundColor('black')
+          }
+          else setBackgroundColor('white')
+        }}
+      >
+        <Text style={styles.text}>Press Here</Text>
+      </TouchableOpacity>
     </View>
   )
 }
-export default HelloWorldApp;
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "rgba(170,170,200,.3)",
+    padding: 10
+  },
+  text:{
+    color: '#9C846A',
+    fontSize: 20,
+    fontFamily: 'calibri',
+    fontStyle: 'italic',
+    fontWeight: 'bold'
+  }
+});
+
+export default App;
