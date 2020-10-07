3820311fd2820e0fd9e12027c027bfca875c8a04
nutunabe <baiytaan@gmail.com>
2020-10-07 22:03:29 +0900

    added icons

--- baiytaanFedorov/App.js
+++ baiytaanFedorov/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from "./screens/home"
-import Lab2 from "./screens/lab2"
-import Lab3 from "./screens/lab3"
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Home from "./screens/home";
+import Lab2 from "./screens/lab2";
+import Lab3 from "./screens/lab3";
 
 const Tab = createBottomTabNavigator();
 
@@ -12,27 +13,42 @@ const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'ℍ𝕆𝕄𝔼') {
+              iconName = 'home-filled';
+            } else if (route.name === '𝕃𝔸𝔹𝟚' || route.name === '𝕃𝔸𝔹𝟛') {
+              iconName = 'format-list-bulleted';
+            }
+            color = focused ? '#fff' : '#dcdcdc';
+            size = focused ? 25 : 22;
+
+            return <MaterialIcons name={iconName} size={size} color={color} />;
+          }
+        })}
         tabBarOptions={{
           style: {
-            height: 40,
+            height: 50,
             backgroundColor: '#757575',
             borderTopWidth: 0.5,
-            borderTopColor: '#fb9800',
+            borderTopColor: '#fb9800'
           },
           labelStyle: {
-            fontSize: 15,
-            color: '#f2f2f2',
+            fontSize: 12,
             fontWeight: 'bold'
           },
           tabStyle: {
             justifyContent: 'center'
           },
-          showIcon: false
+          activeTintColor: '#fff',
+          inactiveTintColor: '#dcdcdc'
         }}
       >
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="𝕃𝔸𝔹𝟚" component={Lab2} />
-        <Tab.Screen name="𝕃𝔸𝔹𝟛" component={Lab3} />
+        <Tab.Screen name='ℍ𝕆𝕄𝔼' component={Home} />
+        <Tab.Screen name='𝕃𝔸𝔹𝟚' component={Lab2} />
+        <Tab.Screen name='𝕃𝔸𝔹𝟛' component={Lab3} />
       </Tab.Navigator>
     </NavigationContainer>
   )
