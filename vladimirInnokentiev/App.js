470c78d1fc02b160e9e783e4050be8f81b78f143
0dj3 <xaryshal@gmail.com>
2020-10-14 13:45:05 +0900

    Added BottomTabNavigator

--- vladimirInnokentiev/App.js
+++ vladimirInnokentiev/App.js
@@ -1,57 +1,27 @@
-import React, {useState} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-  Dimensions,
-} from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Lab2 from './screens/lab2.js'
 
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+const Tab = createBottomTabNavigator();
 
-const {width} = Dimensions.get('screen');
-const SIZE = width * 0.9;
-
-const App: () => React$Node = () => {
-
-  const [color1,setColor1]=useState(Colors.white);
-
-  const randomHex = () => {
-    let letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) 
-    {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  };
-
+const App = () => {
   return (
-    <View style={styles.Block}>
-      <View style = {styles.blockRow}>
-        <TouchableOpacity onPress={() => { setColor1(randomHex()); }}>
-          <View style = {{...styles.body, backgroundColor: color1}}/>
-        </TouchableOpacity>
-      </View>
-    </View>
-  
-    
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Lab2" component={Lab2} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  Block: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: SIZE/2
-  },
-  body: {
-    width: SIZE, 
-    height: SIZE, 
-    borderRadius: SIZE /2
-  },
-});
-
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Welcome to the club buddy!</Text>
+    </View>
+  );
+}
 export default App;
