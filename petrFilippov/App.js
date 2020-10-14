209212e7881182886cb9877c0a0250359c0fdfb1
hondajke <petr.filippov.1999@mail.ru>
2020-10-14 13:02:01 +0900

    Third lab done, +/screens

--- petrFilippov/App.js
+++ petrFilippov/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+/*import React from 'react';
 import {
   StyleSheet,
   ScrollView,
@@ -6,62 +6,78 @@ import {
   Text,
   TouchableOpacity,
 } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import lab2 from "./screens/lab2"
 
+const {width, height} = Dimensions.get('screen')
 
 const styles = StyleSheet.create({
-  plusMinusStyle:{
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    maxWidth: 100,
-    maxHeight: 100,
-    backgroundColor: "purple",
-    borderRadius: 25,
-    marginTop: 10,
-  },
-  centrify:{
-    flex:1,
-    alignItems: "center",
-    flexDirection: "column",
-  },
-  txtContainer:{
-    flex:1,
-    justifyContent: "center",
-    maxHeight: 100,
-    maxWidth: 100,
-  },
-  containers:{
-    flexDirection: "row",
-  },
+
 })
 
+const Tab = createMaterialTopTabNavigator();
+
 const App = () => {
-  const [count, setCount] = useState(0)
+  return(<View>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" />
+      <Tab.Screen name="Settings" />
+    </Tab.Navigator>
+  </View>)
+}
+export default App;*/
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Lab1 from "./screens/lab1"
+import Lab2 from "./screens/lab2"
 
-  const minusPress = () => setCount(prevCount => prevCount - 1);
-  const plusPress = () => setCount(prevCount => prevCount + 1);
 
-  return(
-    <>
-      <ScrollView>
-        <View style={styles.centrify}>
-          <View style={styles.txtContainer}>
-            <Text>Count: {count}</Text>
-          </View>
-          <View style={styles.containers}>
-            <TouchableOpacity onPress={minusPress} style={styles.plusMinusStyle}>
-              <Text>Minus</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={plusPress} style={styles.plusMinusStyle}>
-              <Text>Plus</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </ScrollView>
-    </>
-  )
+const HomeScreen = () => {
+  return (
+    <View style={style.centrify}>
+      <Text>Home!</Text>
+    </View>
+  );
 }
 
+const FirstLab = () => {
+  return (
+    <View style={style.centrify}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
 
+const SecondLab = () => {
+  return (
+    <View style={style.centrify}>
+      <Text>Second Screen</Text>
+    </View>
+  );
+}
 
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="First lab" component={Lab1} />
+        <Tab.Screen name="Second lab" component={Lab2} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const style = StyleSheet.create({
+  centrify: {
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center' 
+  },
+})
 export default App;
