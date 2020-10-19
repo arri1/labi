7b147f5d4446f6f394290772a825843cc85c1954
Mihasta <mihastacom@gmail.com>
2020-10-19 18:25:24 +0900

    lab2

--- mixailNikitin/App.js
+++ mixailNikitin/App.js
@@ -1,31 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
+  TouchableOpacity
 } from 'react-native';
 
-const App: () => React$Node = () => {
+const App = () => {
+
+  const [backgroundColor, setBackgroundColor] = useState('#DCDCDC')
+
+  let randomColorCode=()=>
+  {
+    var ColorCode = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+    return ColorCode;
+  }
+
   return (
-    <View style = {styles.text}>
-      <Text>
-        Я люблю крепкий чай
-      </Text>
+    <View style = {[styles.body, { backgroundColor }]}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          setBackgroundColor(randomColorCode());   
+      }}>
+        <Text style={styles.text}>
+          Change color
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  text: {
-    textAlign: "center",
-    alignItems: "center",
+  body: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#F8F8FF',
+    textAlign: "center",
+    fontSize: 20,
+    fontFamily: "serif",
+  },
+  button: {
+    backgroundColor: '#000000',
+    width: 150,
+    height: 90,
+    borderRadius: 20,
+    padding: 20,
   }
-
 });
 
 export default App;
