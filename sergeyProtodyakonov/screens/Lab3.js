f9737c72071ed2b58d8bd320a3cc61c9c706002a
Rekfuby <trollebane@gmail.com>
2020-10-21 20:08:54 +0900

    changed project structure

---
+++ sergeyProtodyakonov/screens/Lab3.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import {
+    View,
+    Text,
+} from 'react-native';
+
+
+const Lab3 = () => {
+    return (
+        <View style={
+            {
+                flex: 1,
+                justifyContent: 'center', 
+                alignItems: 'center' 
+            }
+        }>
+        <Text>Lab3</Text>
+        </View>
+    );
+}
+
+export default Lab3
