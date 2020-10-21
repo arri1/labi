1e14494e23a09824d6e0576851f31aaaba929ef9
Witawe <texys@list.ru>
2020-10-21 17:43:30 +0900

    edit lab3

--- alexeyLukovtsev/App.js
+++ alexeyLukovtsev/App.js
@@ -8,35 +8,20 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Lab1 from "./screens/lab1"
 import Lab2 from "./screens/lab2"
+import Home from "./screens/home"
 
 const Tab = createBottomTabNavigator();
-
-const txtq = () => {
-  return(
-  <View style={style.txt}>
-    <Text>Ya zdec'</Text>
-  </View>
-  )
-}
 
 const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={txtq} />
+        <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="lab1" component={Lab1} />
         <Tab.Screen name="lab2" component={Lab2} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-const style = StyleSheet.create({
-  txt: {
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center' 
-  },
-})
 
 export default App;
