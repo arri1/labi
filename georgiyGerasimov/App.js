d23a1ee68f694b5413fba06b71fb5f7fced019cd
arri <ewws@mail.ru>
2020-10-26 21:05:24 +0900

    incapsulated bottom router

--- georgiyGerasimov/App.js
+++ georgiyGerasimov/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {ApolloProvider} from '@apollo/react-hooks'
 import apollo from './utils/apollo'
-import Lab2 from "./screens/lab2"
-import Lab3 from "./screens/lab3"
+import BottomRouter from "./routers/bottomRouter"
 
-const Tab = createBottomTabNavigator();
 
 const App = () => {
     return (
         <ApolloProvider client={apollo}>
             <NavigationContainer>
-                <Tab.Navigator>
-                    <Tab.Screen name="Lab2" component={Lab2}/>
-                    <Tab.Screen name="Lab3" component={Lab3}/>
-                </Tab.Navigator>
+                <BottomRouter/>
             </NavigationContainer>
         </ApolloProvider>
 
---
+++ georgiyGerasimov/routers/bottomRouter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
+import Lab2 from "../screens/lab2"
+import Lab3 from "../screens/lab3"
+
+const Tab = createBottomTabNavigator();
+
+const BottomRouter = () => {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Lab2" component={Lab2}/>
+            <Tab.Screen name="Lab3" component={Lab3}/>
+        </Tab.Navigator>
+    )
+}
+export default BottomRouter
