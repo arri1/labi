7ab2b5bd2e680173ef1c45bb917f0e78aa2112d9
aitalyy <aitalyy@gmail.com>
2020-11-30 19:25:46 +0900

    fixed figma

--- aitalEgorov/App.js
+++ aitalEgorov/App.js
@@ -10,7 +10,10 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import Lab2 from "./scripts/lab2";
 import Lab3 from "./scripts/lab3";
-import Title from "./scripts/title"
+import Title from "./scripts/title";
+import signUp from "./scripts/figma/signUp";
+import signIn from "./scripts/figma/signIn";
+
 
 
 
@@ -23,6 +26,8 @@ const App = () =>{
         <Drawer.Screen name="Lab2" component={Lab2} />
         <Drawer.Screen name="Lab3" component={Lab3} />
         <Drawer.Screen name="Title" component={Title} />
+        <Drawer.Screen name="signUp" component={signUp} />
+        <Drawer.Screen name="signIn" component={signIn} />
       </Drawer.Navigator>
     </NavigationContainer>
   )
