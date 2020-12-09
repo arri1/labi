10b8c18d04d367ace8721d11034ec8010ecf0874
arri <ewws@mail.ru>
2020-12-09 15:46:20 +0900

    added posts

--- georgiyGerasimov/routers/bottomRouter.js
+++ georgiyGerasimov/routers/bottomRouter.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
-import Lab2 from "../screens/lab2"
-import Lab3 from "../screens/lab3"
-import Settings from "../screens/settings"
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import Lab2 from '../screens/lab2'
+import Lab3 from '../screens/lab3'
+import Settings from '../screens/settings'
+import PostsRouter from './postsRouter'
 
 const Tab = createBottomTabNavigator();
 
@@ -12,6 +13,7 @@ const BottomRouter = () => {
             <Tab.Screen name="Lab2" component={Lab2}/>
             <Tab.Screen name="Lab3" component={Lab3}/>
             <Tab.Screen name="Settings" component={Settings}/>
+            <Tab.Screen name="PostsRouter" component={PostsRouter}/>
         </Tab.Navigator>
     )
 }
