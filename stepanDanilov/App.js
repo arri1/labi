b46ea48003afc145697155d3dd6a9db7177db325
stepandanilov <5digitnoob@gmail.com>
2020-12-09 11:11:04 +0900

    created basic navigation folder

--- stepanDanilov/App.js
+++ stepanDanilov/App.js
@@ -1,51 +1,12 @@
 import React from 'react'
 import 'react-native-gesture-handler';
-import {View, Text, StyleSheet, Button, Alert} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import Lab2 from './src/screens/lab2'
-import Lab3 from './src/screens/lab3'
+import MainNavigation from './src/navigation/mainNavigation'
 
-const Tab = createBottomTabNavigator();
-const Lab2Stack = createStackNavigator();
-
-const Lab2StackScreen = () =>{
-  return(
-    <Lab2Stack.Navigator>
-      <Lab2Stack.Screen name="Lab2" component={Lab2}/>
-    </Lab2Stack .Navigator>
-  )
-}
-const Lab3StackScreen = () =>{
-  return(
-    <Lab2Stack.Navigator>
-      <Lab2Stack.Screen name="Lab3" component={Lab3}/>
-    </Lab2Stack .Navigator>
-  )
-}
 const App = () => {
-    return (
-      <NavigationContainer>
-          <Tab.Navigator tabBarOptions={{
-            labelStyle:{
-              fontSize:30,
-            }
-          }}>
-              <Tab.Screen name="Lab 2" component={Lab2StackScreen} />
-              <Tab.Screen name="Lab 3" component={Lab3StackScreen} />
-          </Tab.Navigator>
-      </NavigationContainer>        
-    )
+  return(
+    <MainNavigation>
+      
+    </MainNavigation>
+  )
 }
-const styles = StyleSheet.create ({
-   
-  container: {
-    flex: 1,
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize:24,
-  },
-})
 export default App;
