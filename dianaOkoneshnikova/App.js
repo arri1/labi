c79f637cf47ba77acc33a2ba33dc07b746c9b05f
joinfc <romanmail23@gmail.com>
2020-12-09 18:45:07 +0900

    navigator

--- dianaOkoneshnikova/App.js
+++ dianaOkoneshnikova/App.js
@@ -1,35 +1,11 @@
-import React, { useState } from 'react';
-import {
-    StyleSheet,
-    TouchableOpacity,
-    View,
-    Text,
-} from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import BottomTabRouter from './routers/bottomTabRouter.js'
 
-const App = () => {
-    const [backgroundColor, setbackgroundColor] = useState('pink')
+export default function App() {
     return (
-        <View style={styles.container, { backgroundColor }}>
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => {
-                    if (backgroundColor === 'pink') {
-                        setbackgroundColor('purple')
-                        return null
-                    }
-                    setbackgroundColor('pink')
-                }}
-            ></TouchableOpacity>
-        </View>
-    )
+        <NavigationContainer>
+            <BottomTabRouter />
+        </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-    button: {
-        width:"100%",
-        height:"100%",
-        justifyContent: 'center',
-    }
-});
-
-export default App;
