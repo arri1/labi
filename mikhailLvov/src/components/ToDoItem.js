1a5a3a539b0b55d835461f67efc94216c44e0865
xaata <xaata3007@mail.ru>
2020-12-16 18:34:07 +0900

    2,3

---
+++ mikhailLvov/src/components/ToDoItem.js
@@ -0,0 +1,40 @@
+import React ,{useState , useEffect } from 'react';
+import styles from '../styles/styles'
+import {Card} from 'react-native-paper'
+import Checkbox from 'react-native-paper/src/components/Checkbox/Checkbox'
+
+const ToDoItem = (props) => {
+    const [checked, setChecked] = useState(false)
+
+    useEffect(() => {
+        props.item.completed
+            ? setChecked(true)
+            : setChecked(false)
+    }, [])
+
+    return (
+        <Card style={styles.cardStyle}>
+            <Card.Content style={styles.cardContent}>
+                <Card.Title
+                    title={props.item.title}
+                    titleNumberOfLines={2}
+                    right={() => (
+                        <Checkbox
+                            status={
+                                checked
+                                    ? 'checked'
+                                    : 'unchecked'
+                            }
+                            onPress={() => {
+                                setChecked(!checked)
+                            }}
+                            color='#F11F1F'
+                        />
+                    )}
+                />
+            </Card.Content>
+        </Card>
+    )
+};
+
+export default ToDoItem
---
+++ mikhailLvov/src/screens/lab2.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Button, Alert} from 'react-native';
+import styles from '../styles/styles'
+
+const Lab2 = () => {
+  const [colorState, setColorState] = useState('red');
+
+  const onGreenButton = () => {
+      setColorState('green')
+  }
+  const onRedButton = () => {
+      setColorState('red')
+  }
+  const onBlueButton = () => {
+    setColorState('blue')
+  }
+  return (
+      <View style={styles.container}>
+      <Text>Lab 2</Text>
+
+      <View
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          backgroundColor: colorState,
+        }}
+      />
+      <Button title="Red" color="red" onPress={onRedButton} />
+      <Button title="Green" color="green" onPress={onGreenButton} />
+      <Button title="Blue" color="blue" onPress={onBlueButton} />
+    </View>
+  );
+};
+
+export default Lab2
