cd73562b1018b74590abfc5922be6c4945a0c73b
welp1 <minthourwnz@gmail.com>
2020-12-16 16:15:11 +0900

    lab3

--- arsenAmmosov/App.js
+++ arsenAmmosov/App.js
@@ -4,6 +4,7 @@ import {View, Text, StyleSheet, Button, Alert} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Lab2 from './src/lab2'
+import Lab3 from './src/lab3'
 
 const Tab = createBottomTabNavigator();
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <NavigationContainer>
           <Tab.Navigator>
               <Tab.Screen name="Lab 2" component={Lab2} />
+              <Tab.Screen name="Lab 3" component={Lab3} />
           </Tab.Navigator>
       </NavigationContainer>
   )
