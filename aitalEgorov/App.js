edc16c3fa1fe2277f3028b878a1ce85e018bccef
aitalyy <aitalyy@gmail.com>
2020-12-22 04:11:11 +0900

    added todolist

--- aitalEgorov/App.js
+++ aitalEgorov/App.js
@@ -10,10 +10,10 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import Lab2 from "./scripts/lab2";
 import Lab3_2 from "./scripts/lab3_2";
-import Lab3 from "./scripts/components/lab3";
 import Title from "./scripts/title";
 import signUp from "./scripts/figma/signUp";
 import signIn from "./scripts/figma/signIn";
+import toDoList from "./scripts/figma/todo";
 import cat from "./scripts/cat";
 
 
@@ -26,9 +26,9 @@ const App = () =>{
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Lab2">
         <Drawer.Screen name="Lab2" component={Lab2} />
-        <Drawer.Screen name="Lab3" component={Lab3} />
-        <Drawer.Screen name="Lab3_2" component={Lab3_2} />
+        <Drawer.Screen name="Lab3" component={Lab3_2} />
         <Drawer.Screen name="Title" component={Title} />
+        <Drawer.Screen name="To do list" component={toDoList} />
         <Drawer.Screen name="signUp" component={signUp} />
         <Drawer.Screen name="signIn" component={signIn} />
         <Drawer.Screen name="cat" component={cat} />
