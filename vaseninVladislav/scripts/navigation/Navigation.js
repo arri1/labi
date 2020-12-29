a245de9b3348b15fb1b95c0781ddb3e6d979c99c
fiitt <vvnfiit@gmail.com>
2020-12-29 22:40:23 +0900

    added lab3

---
+++ vaseninVladislav/scripts/navigation/Navigation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Dimensions, StyleSheet } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Lab2 from "../screens/lab2"
+import Lab3 from "../screens/lab3"
+
+const Tab = createBottomTabNavigator();
+
+const Navigation = () => {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen 
+                    name="Lab2" 
+                    component={Lab2} 
+                />
+                <Tab.Screen 
+                    name="Lab3" 
+                    component={Lab3} 
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+
+    )
+}
+
+export default Navigation;
