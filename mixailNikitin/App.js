d6bb7a8198b4ced2470f7ab7676d72f788c55b19
Mihasta <mihastacom@gmail.com>
2020-12-29 12:55:05 +0900

    added lab4

--- mixailNikitin/App.js
+++ mixailNikitin/App.js
@@ -10,6 +10,7 @@ import {
 import HomeScreen from './screens/HomeScreen';
 import Lab2 from './screens/Lab2';
 import Lab3 from './screens/Lab3';
+import Lab4 from './screens/Lab4';
 
 const Tab = createBottomTabNavigator();
 
@@ -38,6 +39,7 @@ const App = () => {
         <Tab.Screen name='HomeScreen' component={HomeScreen} />
         <Tab.Screen name='Lab2' component={Lab2} />
         <Tab.Screen name='Lab3' component={Lab3} />
+        <Tab.Screen name='Lab4' component={Lab4} />
       </Tab.Navigator>
     </NavigationContainer>
   );
