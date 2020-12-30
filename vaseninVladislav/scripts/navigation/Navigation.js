59335cb53ab1e4b2c622cce0e4d224129c1cc4b6
fiitt <vvnfiit@gmail.com>
2020-12-30 21:56:43 +0900

    added lab4 todo list с картинками

--- vaseninVladislav/scripts/navigation/Navigation.js
+++ vaseninVladislav/scripts/navigation/Navigation.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Lab2 from "../screens/lab2"
 import Lab3 from "../screens/lab3"
+import Lab4 from "../screens/lab4"
 
 const Tab = createBottomTabNavigator();
 
@@ -18,6 +19,10 @@ const Navigation = () => {
                 <Tab.Screen 
                     name="Lab3" 
                     component={Lab3} 
+                />
+                <Tab.Screen 
+                    name="Lab4" 
+                    component={Lab4} 
                 />
             </Tab.Navigator>
         </NavigationContainer>
