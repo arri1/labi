c432236ca763263871975c29c02895e308d86d75
joinfc <romanmail23@gmail.com>
2020-12-31 02:48:32 +0900

    lab 5 test

--- dianaOkoneshnikova/App.js
+++ dianaOkoneshnikova/App.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import 'react-native-gesture-handler';
-import MainNavigation from './src/routers/bottomTabRouter'
+import MainNavigation from './src/routers/mainRouter'
 
 const App = () => {
-  global.foo = {uri : 'https://images.unsplash.com/photo-1556861460-7d38b2955d05?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1534&q=80'}
-  return(
-    <MainNavigation>
-      
-    </MainNavigation>
-  )
+    return (
+        <View style={
+            {
+                width,
+                height
+            }
+        }>
+            <ApolloProvider client={apollo}>
+                <NavigationContainer>
+                    <MainRouter/>
+                </NavigationContainer>
+            </ApolloProvider>
+            <FlashMessage position="top"/>
+        </View>
+    )
 }
 export default App;
---
+++ dianaOkoneshnikova/src/routers/mainRouter.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {createStackNavigator} from '@react-navigation/stack'
+import BottomTabRouter from "./bottomTabRouter"
+import Login from "../screens/login"
+import Registration from "../screens/registration"
+
+const Stack = createStackNavigator()
+
+const mainRouter = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name={'Login'}
+                options={
+                    {
+                        title: 'Вход'
+                    }
+                }
+                component={Login}/>
+            <Stack.Screen
+                name={'Registration'}
+                options={
+                    {
+                        title: 'Регистрация'
+                    }
+                }
+                component={Registration}/>
+            <Stack.Screen
+                name={'BottomTabRouter'}
+                component={BottomTabRouter}
+                options={
+                    {headerShown: false}
+                }
+            />
+        </Stack.Navigator>
+    )
+}
+export default mainRouter
