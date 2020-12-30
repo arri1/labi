b899721a0029872f786100889498f0ddb049fe86
welp1 <minthourwnz@gmail.com>
2020-12-30 19:18:24 +0900

    lab4 figma

---
+++ arsenAmmosov/src/components/backgroundBox.js
@@ -0,0 +1,29 @@
+import React, {useState , useEffect} from 'react';
+import {ImageBackground, TouchableOpacity, View} from 'react-native'
+import styles from "../styles/styles"
+
+const image1 = { uri : 'https://photocasa.ru/uploads/posts/2017-01/1485218882_img_5932.jpg'};
+const image2 = {uri : 'https://www.fotolandscape.com/wp-content/uploads/2014/12/DSC_0225.jpg'};
+
+const background = (image) =>{
+    if (image == "1") return image1
+    else return image2
+}
+const BackgroundBox = (props) =>{
+    return (
+        <View style = {styles.containerLab2}>
+            <TouchableOpacity
+            style = {styles.itemLab2}
+            onPress={props.onPress}>
+                <ImageBackground
+                style = {styles.backgroundImage}
+                imageStyle={{
+                    borderRadius: 20
+                }}
+                source = {background(props.image)}>
+                </ImageBackground>
+            </TouchableOpacity>
+        </View>
+    )
+}
+export default BackgroundBox
---
+++ arsenAmmosov/src/navigation/mainNavigation.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Lab2StackScreen from '../stackNavigators/Lab2StackScreen'
+import Lab3StackScreen from '../stackNavigators/Lab3StackScreen'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import 'react-native-gesture-handler';
+import styles from '../styles/styles'
+
+const MainNavigation = () => {
+    const Tab = createBottomTabNavigator();
+    return (
+      <NavigationContainer>
+          <Tab.Navigator 
+          tabBarOptions={{
+            labelStyle:{
+              ...styles.navTabLabel,
+              color:'white'
+            },
+            tabStyle:{
+              backgroundColor:"#181818"
+            }
+          }}
+          >
+              <Tab.Screen name="Lab 1" component={Lab2StackScreen} 
+              options={{
+                tabBarLabel: 'Фон'
+              }} />
+              <Tab.Screen name="Lab 2" component={Lab3StackScreen} 
+              options={{
+                tabBarLabel: 'Заметки'
+              }}/>
+          </Tab.Navigator>
+      </NavigationContainer>        
+    )
+}
+export default MainNavigation
