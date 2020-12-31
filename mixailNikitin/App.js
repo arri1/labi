06ffacb9679286662443469761b870442d5afe13
Mihasta <mihastacom@gmail.com>
2020-12-31 12:03:25 +0900

    added lab5

--- mixailNikitin/App.js
+++ mixailNikitin/App.js
@@ -1,48 +1,28 @@
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-} from 'react-native';
-import HomeScreen from './screens/HomeScreen';
-import Lab2 from './screens/Lab2';
-import Lab3 from './screens/Lab3';
-import Lab4 from './screens/Lab4';
+import {Dimensions, View} from "react-native"
+import {NavigationContainer} from '@react-navigation/native';
+import {ApolloProvider} from '@apollo/react-hooks'
+import FlashMessage from "react-native-flash-message";
+import apollo from './utils/apollo'
+import MainRouter from "./routers/mainRouter"
 
-const Tab = createBottomTabNavigator();
+const {width, height} = Dimensions.get('screen')
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        tabBarOptions={{
-          style: {
-            height: 50,
-            backgroundColor: '#000000',
-            borderTopWidth: 1,
-            borderTopColor: '#F8F8FF',
-          },
-          labelStyle: {
-            fontSize: 17,
-            fontFamily: 'serif',
-          },
-          tabStyle: {
-            justifyContent: 'center'
-          },
-          activeTintColor: '#8A2BE2',
-          inactiveTintColor: '#F8F8FF'
-        }}
-      >
-        <Tab.Screen name='HomeScreen' component={HomeScreen} />
-        <Tab.Screen name='Lab2' component={Lab2} />
-        <Tab.Screen name='Lab3' component={Lab3} />
-        <Tab.Screen name='Lab4' component={Lab4} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-};
-
+    return (
+        <View style={
+            {
+                width,
+                height
+            }
+        }>
+            <ApolloProvider client={apollo}>
+                <NavigationContainer>
+                    <MainRouter/>
+                </NavigationContainer>
+            </ApolloProvider>
+            <FlashMessage position="top"/>
+        </View>
+    )
+}
 export default App;
