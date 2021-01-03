92599613eb52a3ca1a4032137f7746484b8c695d
fiitt <vvnfiit@gmail.com>
2021-01-03 19:47:15 +0900

    added lab5

--- vaseninVladislav/scripts/screens/lab4.js
+++ vaseninVladislav/scripts/screens/lab4.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, { useEffect, useState } from 'react'
 import {
     ActivityIndicator,
@@ -21,7 +13,7 @@ import axios from 'react-native-axios'
 const Lab4 = () => {
     const [data, setData] = useState()
     useEffect(() => {
-        axios.get('https://picsum.photos/v2/list?limit=20')
+        axios.get('https://picsum.photos/v2/list?limit=10')
         .then(({ data }) => {
             setData(data)
         })
