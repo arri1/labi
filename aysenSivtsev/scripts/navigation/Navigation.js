1ae98c2a60bdb5fa6bee6c1d253b6fe022c962e7
D-iardan <D-iardan@yandex.ru>
2021-05-16 12:14:08 +0900

    deleted 4 lab

--- aysenSivtsev/scripts/navigation/Navigation.js
+++ aysenSivtsev/scripts/navigation/Navigation.js
@@ -4,7 +4,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Lab2 from "../screens/lab2"
 import Lab3 from "../screens/lab3"
-import Lab4 from "../screens/lab4"
 
 const Tab = createBottomTabNavigator();
 
@@ -19,10 +18,6 @@ const Navigation = () => {
                 <Tab.Screen 
                     name="Lab3" 
                     component={Lab3} 
-                />
-                <Tab.Screen 
-                    name="Lab4" 
-                    component={Lab4} 
                 />
             </Tab.Navigator>
         </NavigationContainer>
