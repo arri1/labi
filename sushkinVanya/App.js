30c991c4d91bd8f7950acd3235d09ccde2249957
VanyaSushkin <>
2021-05-17 19:59:20 +0900

    Revert "lab 5"
    This reverts commit 79533f27824470234acd26c772a7363d6f7633db.

--- sushkinVanya/App.js
+++ sushkinVanya/App.js
@@ -1,32 +1,26 @@
-import React from 'react';
-import {Dimensions, View} from "react-native"
-import {NavigationContainer} from '@react-navigation/native';
-import {ApolloProvider} from '@apollo/react-hooks'
-import FlashMessage from "react-native-flash-message";
-import apollo from './utils/apollo'
-import Lab5 from './scripts/screens/lab5'
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import Lab2 from "./components/Lab2";
+import Lab3 from "./components/Lab3";
+import Lab4 from "./components/Lab4";
+import MainPage from "./components/MainPage";
 
-const {width, height} = Dimensions.get('screen')
+const RootStack = createStackNavigator(
+  {
+    MainPage: MainPage,
+    Lab2: Lab2,
+    Lab3: Lab3,
+    Lab4: Lab4,
+  },
+);
 
+const AppContainer = createAppContainer(RootStack);
 
-const App = () => {
+export default class App extends React.Component {
 
-    return (
-        <View style={
-            {
-                width,
-                height
-            }
-        }>
-            <ApolloProvider client={apollo}>
-                <NavigationContainer>
-                    <Lab5/>
-                </NavigationContainer>
-            </ApolloProvider>
-            <FlashMessage position="top"/>
-        </View>
-    )
-};
-
-
-export default App;
+  
+  render() {
+    return <AppContainer />;
+  }
+}
