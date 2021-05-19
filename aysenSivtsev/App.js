5ddb9d93c7bf87783ab33d0ae03b3c88c837e02e
D-iardan <D-iardan@yandex.ru>
2021-05-19 15:29:19 +0900

    added 5 lab

--- aysenSivtsev/App.js
+++ aysenSivtsev/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import Navigation from './scripts/navigation/Navigation'
+import {Dimensions, View} from "react-native"
+import {NavigationContainer} from '@react-navigation/native';
+import {ApolloProvider} from '@apollo/react-hooks'
+import FlashMessage from "react-native-flash-message";
+import apollo from './utils/apollo'
+import Lab5 from './scripts/screens/lab5'
+
+const {width, height} = Dimensions.get('screen')
 
 const App = () => {
 
-    return <Navigation />
-    
-}
+    return (
+        <View style={
+            {
+                width,
+                height
+            }
+        }>
+            <ApolloProvider client={apollo}>
+                <NavigationContainer>
+                    <Lab5/>
+                </NavigationContainer>
+            </ApolloProvider>
+            <FlashMessage position="top"/>
+        </View>
+    )
+};
 
 export default App;
 
