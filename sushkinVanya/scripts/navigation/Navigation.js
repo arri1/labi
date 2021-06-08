9005a61ec45fa679d849e16315fd9089cacfbe0b
VanyaSushkin <svfuivan2@gmail.com>
2021-06-08 20:00:05 +0900

    lab 5 fixes

--- sushkinVanya/scripts/navigation/Navigation.js
+++ sushkinVanya/scripts/navigation/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dimensions, StyleSheet } from 'react-native'
+import { Dimensions, StyleSheet, SafeAreaView } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Lab2 from "../../components/Lab2"
@@ -11,38 +11,42 @@ const Tab = createBottomTabNavigator();
 
 const Navigation = () => {
     return (
-        <Tab.Navigator
-            tabBarOptions={{
-                labelStyle: {
-                    tTransform: "none",
-                    fontSize: 25,
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center"
-				},
-                style:{
-                  borderTopWidth: 2,
-                  borderBottomWidth: 2,
-                }
-          }}
-        >
-            <Tab.Screen 
-                name="Lab2" 
-                component={Lab2} 
-            />
-            <Tab.Screen 
-                name="Lab3" 
-                component={Lab3} 
-            />
-            <Tab.Screen 
-                name="Lab4" 
-                component={Lab4} 
-            />
-            <Tab.Screen 
-                name="Lab5" 
-                component={Lab5} 
-            />
-        </Tab.Navigator>
+        <SafeAreaView style= {
+                {
+                flex:1
+            }
+        }>
+            <Tab.Navigator
+                tabBarOptions={{
+                    labelStyle: {
+                        fontSize: 25,
+                        flex: 1,
+                        justifyContent: "center",
+                        alignItems: "center"
+                    },
+                    style:{
+                    marginBottom: 20
+                    }
+            }}
+            >
+                <Tab.Screen 
+                    name="Lab2" 
+                    component={Lab2} 
+                />
+                <Tab.Screen 
+                    name="Lab3" 
+                    component={Lab3} 
+                />
+                <Tab.Screen 
+                    name="Lab4" 
+                    component={Lab4} 
+                />
+                <Tab.Screen 
+                    name="Lab5" 
+                    component={Lab5} 
+                />
+            </Tab.Navigator>
+        </SafeAreaView>
     )
 }
 
